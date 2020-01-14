98c6f38a9bc2e5ec8af9c583a21c918d8d41acf6
augustinboju <bojuaugustin@gmail.com>
2020-01-14 13:58:25 +0100

    ok

--- ex_20.rb
+++ ex_20.rb
@@ -1,7 +1,6 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 etages = gets.chomp.to_i
 puts "Voici la pyramide :"
-hastag = "#"
 size = 1
 while size <= etages do
   puts "#" * size
